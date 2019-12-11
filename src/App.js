b32675ce92da23d585f41b135283e76776e09a8d
TBM85 <tballesterm@gmail.com>
2019-12-11 18:40:43 -0500

    Make a Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './scss/App.scss';
 
 const App = () => {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { menu_items } from '../store';
+
+class Header extends Component {
+  render() {
+    return(
+      <header id="header">
+        <nav className="navbar navbar-expand-lg">
+          <span>World Travel</span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" onClick={this.handleClick}>
+            <FaBars />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              {menu_items.map((menu_item, i) => (
+                <li key={i} className="nav-item active">
+                  <a className="nav-link" href={menu_item.href}>{menu_item.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
+
+        <div className="header-content">
+          <h1>World Travel</h1>
+          <h5>Enjoy Life Traveling</h5>
+          <a href="#contact" className="btn btn-dark-blue btn-lg" role="button" aria-pressed="true">Contact Us</a>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
