1e65237d0e03d02b3a28d1bd6461cba8b01510bc
TBM85 <tballesterm@gmail.com>
2019-12-22 14:10:31 -0500

    Updates

---
+++ precache-manifest.4f438a8c481832a62bb418bfe99175fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428b21fee6aef95a8efaa2b0bb4dfa9d",
+    "url": "/Demo-World-Travel/index.html"
+  },
+  {
+    "revision": "6e66faea6fcf502bcf61",
+    "url": "/Demo-World-Travel/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "55aa70bb4eb5f8eeced6",
+    "url": "/Demo-World-Travel/static/css/main.abb829cc.chunk.css"
+  },
+  {
+    "revision": "6e66faea6fcf502bcf61",
+    "url": "/Demo-World-Travel/static/js/2.72788468.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Demo-World-Travel/static/js/2.72788468.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55aa70bb4eb5f8eeced6",
+    "url": "/Demo-World-Travel/static/js/main.0c19cb77.chunk.js"
+  },
+  {
+    "revision": "85f56592b85ef05cd0d9",
+    "url": "/Demo-World-Travel/static/js/runtime-main.52e823c9.js"
+  }
+]);
